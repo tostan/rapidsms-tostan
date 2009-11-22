853951ee937a1e69262fb65f54877cb57a1f44c6
rowenaluk <rluk@dimagi.com>
2009-11-22 09:29:21 -0500

    Changed weird unimplemented functions to correctly raise NotImplementedError's

--- apps/locations/models.py
+++ apps/locations/models.py
@@ -45,10 +45,10 @@ class Location(models.Model):
     #       even if they can import one another, they can't know if they're
     #       both running at parse time, and can't monkey-patch later.
     def one_contact(self, role, display=False):
-        return "Mr. Fixme"
+        raise NotImplementedError
 
     def contacts(self, role=None):
-        return Location.objects.get(pk=2)
+        raise NotImplementedError
     
     def ancestors(self, include_self=False):
         """Returns all of the parent locations of this location,

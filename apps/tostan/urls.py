95d0138667d28b90d4074eb076a05911733bca11
rowenaluk <rluk@dimagi.com>
2009-11-28 12:20:30 -0600

    add login_required to direct_to_template views

--- apps/tostan/urls.py
+++ apps/tostan/urls.py
@@ -2,11 +2,16 @@
 # vim: ai ts=4 sts=4 et sw=4
 
 from django.conf.urls.defaults import *
+from django.contrib.auth.decorators import login_required
+from django.views.generic.simple import direct_to_template
 import apps.tostan.views as views
 
-urlpatterns = patterns('django.views.generic.simple',
-    url(r'^$',    'direct_to_template', {'template':"tostan/dashboard.html"}),
-    url(r'^help$',    'direct_to_template', {'template':"tostan/smscommands.html"}, name="help"),
+urlpatterns = patterns('',
+    url(r'^$',         login_required(direct_to_template), 
+                       {'template':"tostan/dashboard.html"}),
+    url(r'^help$',     login_required(direct_to_template), 
+                       {'template':"tostan/smscommands.html"}, name="help"),
     url(r'^export$',   views.export, name="export"),
-    url(r'^404$',       'direct_to_template', {'template':"tostan/404.html"}, name="404"),
+    url(r'^404$',      login_required(direct_to_template), 
+                       {'template':"tostan/404.html"}, name="404"),
 )

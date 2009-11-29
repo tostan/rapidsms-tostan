854b6051d7559b587cdf5f9a8a1884b49f2ecac0
U-rotop\rowena <rowena@rotop.(none)>
2009-11-29 20:12:11 +0100

    Add django_extensions to rapidsms-tostan (mostly for runscript, but other utils come in handy too)

--- apps/django_extensions/management/commands/show_urls.py
+++ apps/django_extensions/management/commands/show_urls.py
@@ -7,7 +7,7 @@ except ImportError:
     # fall back to trunk, pre-NFA merge
     from django.contrib.admin.views.doc import extract_views_from_urlpatterns, simplify_regex
         
-from extensions.management.color import color_style
+from django_extensions.management.color import color_style
 
 class Command(BaseCommand):
     help = "Displays all of the url matching routes for the project."
@@ -27,10 +27,18 @@ class Command(BaseCommand):
         
         views = []
         for settings_mod in settings_modules:
-            urlconf = __import__(settings_mod.ROOT_URLCONF, {}, {}, [''])
+            try:
+                urlconf = __import__(settings_mod.ROOT_URLCONF, {}, {}, [''])
+            except Exception, e:
+                if options.get('traceback', None):
+                    import traceback
+                    traceback.print_exc()
+                print style.ERROR("Error occurred while trying to load %s: %s" % (settings_mod.ROOT_URLCONF, str(e)))
+                continue
             view_functions = extract_views_from_urlpatterns(urlconf.urlpatterns)
             for (func, regex) in view_functions:
-                views.append("%(url)s\t%(module)s.%(name)s" % {'name': style.MODULE_NAME(func.__name__),
+                func_name = hasattr(func, '__name__') and func.__name__ or repr(func)
+                views.append("%(url)s\t%(module)s.%(name)s" % {'name': style.MODULE_NAME(func_name),
                                        'module': style.MODULE(func.__module__),
                                        'url': style.URL(simplify_regex(regex))})
         

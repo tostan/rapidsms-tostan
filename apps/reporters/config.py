5aeb7c67b0f2889d768712b064f9d382138d993f
U-rotop\rowena <rowena@rotop.(none)>
2009-11-29 22:10:11 +0100

    Hide reporters tab. Messy for now; should clean up to use reporters.permissions.can_view

--- apps/reporters/config.py
+++ apps/reporters/config.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 # vim: ai ts=4 sts=4 et sw=4
 
-title = "Reporters and Groups"
-tab_link = "/reporters"
+# hide reporter view for Tostan
+# TODO: hide this tab using can_view permissions
+# title = "Reporters and Groups"
+# tab_link = "/reporters"

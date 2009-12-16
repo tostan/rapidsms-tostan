e5dcbebe54fb9d826e13fc05aa1d5485ff7e903e
alioune <alioune@alioune-laptop.(none)>
2009-12-16 12:00:40 +0100

    New  tested  comment  by alioune

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
-# Test  commentaire     by alioune  
+
+# Test  commentaire     by alioune updated   
 # RapidSMS setup.py -- adapted from Django's with our heartfelt thanks
 from distutils.core import setup
 from distutils.command.build_py import build_py as _build_py
@@ -7,7 +8,6 @@ from distutils.command.install import INSTALL_SCHEMES
 import os
 import sys
 import commands
-# commentaire  de  alioune 
 
 def fullsplit(path, result=None):
     """
